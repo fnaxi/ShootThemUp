41a4f8fc42e10bad42e4e4c4b55586c20f99f6e0
fnaxi <fnanex.74@gmail.com>
2025-05-17 17:37:31 +0300

    Add dev crosshair

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -5,10 +5,12 @@
 
 #include "Player/STUBaseCharacter.h"
 #include "Player/STUPlayerController.h"
+#include "UI/STUGameHUD.h"
 
 ASTUGameModeBase::ASTUGameModeBase()
 {
 	DefaultPawnClass = ASTUBaseCharacter::StaticClass();
 	PlayerControllerClass = ASTUPlayerController::StaticClass();
+	HUDClass = ASTUGameHUD::StaticClass();
 }
 
---
+++ Source/ShootThemUp/Public/UI/STUGameHUD.h
@@ -0,0 +1,25 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "STUGameHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUGameHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	/** The Main Draw loop for the hud. Gets called before any messaging. Should be subclassed. */
+	virtual void DrawHUD() override;
+
+private:
+	/** Draw the crosshair. */
+	void DrawCrosshair();
+	
+};
