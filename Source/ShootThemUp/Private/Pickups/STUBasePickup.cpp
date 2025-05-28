90c5c8b8dffe71397f5d91ce47fca2b97fab3f14
fnaxi <fnanex.74@gmail.com>
2025-05-28 19:48:09 +0300

    Refactoring

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -46,7 +46,7 @@ void ASTUBasePickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
+	AddActorLocalRotation(FRotator(0.0f, RotationYaw * DeltaTime, 0.0f));
 }
 
 bool ASTUBasePickup::GivePickupTo(APawn* PlayerPawn)
@@ -81,6 +81,6 @@ void ASTUBasePickup::Respawn()
 void ASTUBasePickup::GenerateRotationYaw()
 {
 	const float Direction = FMath::RandBool() ? 1.0f : -1.0f;
-	RotationYaw = FMath::RandRange(2.0f, 2.5f) * Direction;
+	RotationYaw = FMath::RandRange(100.0f, 150.0f) * Direction;
 }
 
